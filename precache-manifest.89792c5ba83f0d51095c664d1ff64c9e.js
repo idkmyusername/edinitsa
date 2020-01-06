7b8bb69f606a320ac58f1b391596703e4d454e4a
Anonymous <tarasrusnakbelarus@gmail.com>
2020-01-06 11:56:53 +0300

    Updates

---
+++ precache-manifest.89792c5ba83f0d51095c664d1ff64c9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "000830f640c23ccb5fd4efbeab1a5fe0",
+    "url": "/edinitsa/index.html"
+  },
+  {
+    "revision": "49788bcd4e7fa25dc7bd",
+    "url": "/edinitsa/static/css/2.4e89ce74.chunk.css"
+  },
+  {
+    "revision": "2df92985fc425ced4827",
+    "url": "/edinitsa/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "49788bcd4e7fa25dc7bd",
+    "url": "/edinitsa/static/js/2.2501f0ad.chunk.js"
+  },
+  {
+    "revision": "2df92985fc425ced4827",
+    "url": "/edinitsa/static/js/main.d3a22cd6.chunk.js"
+  },
+  {
+    "revision": "02ea4541e436944ecbe6",
+    "url": "/edinitsa/static/js/runtime-main.9ba7623f.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/edinitsa/static/media/persik.4e1ec840.png"
+  }
+]);
